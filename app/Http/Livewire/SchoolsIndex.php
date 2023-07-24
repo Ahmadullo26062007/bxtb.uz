713e0c0ffd8b454e9019ce38cd32ce04237e0f0b
Ahmadullo26062007 <113325761+Ahmadullo26062007@users.noreply.github.com>
2023-07-24 08:28:07 +0500

    ssdd

--- app/Http/Livewire/SchoolsIndex.php
+++ app/Http/Livewire/SchoolsIndex.php
@@ -7,15 +7,16 @@ use Livewire\Component;
 
 class SchoolsIndex extends Component
 {
-    public $count=8;
+    public $count = 8;
 
     function pilus()
     {
-        $this->count=$this->count+4;
+        $this->count = $this->count + 4;
     }
+
     public function render()
     {
-        $schools= About::take($this->count)->with('users')->orderByDesc('id')->get();
-        return view('livewire.schools-index',compact('schools'));
+        $schools = About::take($this->count)->with('users')->orderByDesc('id')->get();
+        return view('livewire.schools-index', compact('schools'));
     }
 }
