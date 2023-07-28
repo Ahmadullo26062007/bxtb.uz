7567712ce3e328c815dd6f54b956b92bd549a3bd
Ahmadullo26062007 <113325761+Ahmadullo26062007@users.noreply.github.com>
2023-07-28 10:45:33 +0500

    rasm boldi3

--- resources/views/livewire/frontend/teachers-table.blade.php
+++ resources/views/livewire/frontend/teachers-table.blade.php
@@ -22,14 +22,50 @@
 
 {{--                            @php if($c++ == 1)continue; @endphp--}}
 {{--                            @if($c<=20)--}}
-                        <div>
-                            <div class="teacher">
-                                <div class="teacher-img">
-                                    <div style="height: 300px">
+{{--                        <div>--}}
+{{--                            <div class="teacher">--}}
+{{--                                <div class="teacher-img">--}}
+{{--                                    <div style="height: 300px">--}}
 
-                                        <img style="width: 225px !important; height: 300px !important; border-radius: 18%" src="{{ $teacher->image}}" alt="" class="w-100">
+{{--                                        <img style="width: 225px !important; height: 300px !important; border-radius: 18%" src="{{ $teacher->image}}" alt="" class="w-100">--}}
+{{--                                    </div>--}}
+
+{{--                                    <div class="teacher-info">--}}
+{{--                                        <p>--}}
+{{--                                            @php--}}
+{{--                                                $a=\App\Models\About::find($teacher->school_id);--}}
+{{--                                            @endphp--}}
+{{--                                            {{$a->name}} O'qtuvchisi:--}}
+{{--                                        </p>--}}
+{{--                                        <h3><a href="#" title="">{{ $teacher->firstname }}--}}
+{{--                                                {{ $teacher->lastname }}</a></h3>--}}
+{{--                                        <span>{{ $teacher->category }}--}}
+{{--                                            O'qituvchisi</span>--}}
+{{--                                        @if (empty($teacher->degrees[0]))--}}
+{{--                                            <span class="text-dark">--}}
+{{--                                                Birinchi toifa--}}
+{{--                                            </span>--}}
+{{--                                        @else--}}
+{{--                                            @foreach ($teacher->degrees as $degree)--}}
+{{--                                                <span class="text-dark">--}}
+{{--                                                    {{ App\Models\Degree::TYPES[$degree->type_id] }}--}}
+
+{{--                                                </span>--}}
+{{--                                            @endforeach--}}
+{{--                                        @endif--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                                <!--teacher end-->--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                            @endif--}}
+                            <div class="col-lg-3 col-md-3 col-sm-6 col-6 full-wdth">
+                                <div class="teacher">
+                                    <div class="teacher-img"><img style="width: 235px !important; height: 425px; !important;"
+                                                                  src="{{$teacher->image }}" alt=""
+                                                                  class="w-100">
+
                                     </div>
-
                                     <div class="teacher-info">
                                         <p>
                                             @php
@@ -37,28 +73,14 @@
                                             @endphp
                                             {{$a->name}} O'qtuvchisi:
                                         </p>
-                                        <h3><a href="#" title="">{{ $teacher->firstname }}
+                                        <h3><a title="">{{ $teacher->firstname }}
                                                 {{ $teacher->lastname }}</a></h3>
-                                        <span>{{ $teacher->category }}
-                                            O'qituvchisi</span>
-                                        @if (empty($teacher->degrees[0]))
-                                            <span class="text-dark">
-                                                Birinchi toifa
-                                            </span>
-                                        @else
-                                            @foreach ($teacher->degrees as $degree)
-                                                <span class="text-dark">
-                                                    {{ App\Models\Degree::TYPES[$degree->type_id] }}
-
-                                                </span>
-                                            @endforeach
-                                        @endif
+                                        <span>{{ $teacher->category }} O`qituvchisi</span>
                                     </div>
                                 </div>
                                 <!--teacher end-->
                             </div>
-                        </div>
-{{--                            @endif--}}
+
                          @endif
                     @endforeach
                 </div>
