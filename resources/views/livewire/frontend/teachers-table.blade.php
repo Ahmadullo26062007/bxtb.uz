74697c325a7ee0dfae49cfa366c0628f11504384
Ahmadullo26062007 <113325761+Ahmadullo26062007@users.noreply.github.com>
2023-08-01 00:24:18 +0500

    fsewdsaddeqwdwqdwqfefew

--- resources/views/livewire/frontend/teachers-table.blade.php
+++ resources/views/livewire/frontend/teachers-table.blade.php
@@ -18,52 +18,53 @@
             <div class="teachers">
                 <div class="row griddd">
                     @foreach ($teachers as $teacher)
-                        @if(teacher_b($teacher) )
+                        {{--                        @if(teacher_b($teacher) )--}}
+                        @if($teacher->greate_teacher==1)
+                            {{--                            @php if($c++ == 1)continue; @endphp--}}
+                            {{--                            @if($c<=20)--}}
+                            {{--                        <div>--}}
+                            {{--                            <div class="teacher">--}}
+                            {{--                                <div class="teacher-img">--}}
+                            {{--                                    <div style="height: 300px">--}}
 
-{{--                            @php if($c++ == 1)continue; @endphp--}}
-{{--                            @if($c<=20)--}}
-{{--                        <div>--}}
-{{--                            <div class="teacher">--}}
-{{--                                <div class="teacher-img">--}}
-{{--                                    <div style="height: 300px">--}}
+                            {{--                                        <img style="width: 225px !important; height: 300px !important; border-radius: 18%" src="{{ $teacher->image}}" alt="" class="w-100">--}}
+                            {{--                                    </div>--}}
 
-{{--                                        <img style="width: 225px !important; height: 300px !important; border-radius: 18%" src="{{ $teacher->image}}" alt="" class="w-100">--}}
-{{--                                    </div>--}}
+                            {{--                                    <div class="teacher-info">--}}
+                            {{--                                        <p>--}}
+                            {{--                                            @php--}}
+                            {{--                                                $a=\App\Models\About::find($teacher->school_id);--}}
+                            {{--                                            @endphp--}}
+                            {{--                                            {{$a->name}} O'qtuvchisi:--}}
+                            {{--                                        </p>--}}
+                            {{--                                        <h3><a href="#" title="">{{ $teacher->firstname }}--}}
+                            {{--                                                {{ $teacher->lastname }}</a></h3>--}}
+                            {{--                                        <span>{{ $teacher->category }}--}}
+                            {{--                                            O'qituvchisi</span>--}}
+                            {{--                                        @if (empty($teacher->degrees[0]))--}}
+                            {{--                                            <span class="text-dark">--}}
+                            {{--                                                Birinchi toifa--}}
+                            {{--                                            </span>--}}
+                            {{--                                        @else--}}
+                            {{--                                            @foreach ($teacher->degrees as $degree)--}}
+                            {{--                                                <span class="text-dark">--}}
+                            {{--                                                    {{ App\Models\Degree::TYPES[$degree->type_id] }}--}}
 
-{{--                                    <div class="teacher-info">--}}
-{{--                                        <p>--}}
-{{--                                            @php--}}
-{{--                                                $a=\App\Models\About::find($teacher->school_id);--}}
-{{--                                            @endphp--}}
-{{--                                            {{$a->name}} O'qtuvchisi:--}}
-{{--                                        </p>--}}
-{{--                                        <h3><a href="#" title="">{{ $teacher->firstname }}--}}
-{{--                                                {{ $teacher->lastname }}</a></h3>--}}
-{{--                                        <span>{{ $teacher->category }}--}}
-{{--                                            O'qituvchisi</span>--}}
-{{--                                        @if (empty($teacher->degrees[0]))--}}
-{{--                                            <span class="text-dark">--}}
-{{--                                                Birinchi toifa--}}
-{{--                                            </span>--}}
-{{--                                        @else--}}
-{{--                                            @foreach ($teacher->degrees as $degree)--}}
-{{--                                                <span class="text-dark">--}}
-{{--                                                    {{ App\Models\Degree::TYPES[$degree->type_id] }}--}}
-
-{{--                                                </span>--}}
-{{--                                            @endforeach--}}
-{{--                                        @endif--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-{{--                                <!--teacher end-->--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                            @endif--}}
+                            {{--                                                </span>--}}
+                            {{--                                            @endforeach--}}
+                            {{--                                        @endif--}}
+                            {{--                                    </div>--}}
+                            {{--                                </div>--}}
+                            {{--                                <!--teacher end-->--}}
+                            {{--                            </div>--}}
+                            {{--                        </div>--}}
+                            {{--                            @endif--}}
                             <div class="col-lg-3 col-md-3 col-sm-6 col-6 full-wdth">
                                 <div class="teacher">
-                                    <div class="teacher-img"><img style="width: 235px !important; height: 425px; !important;"
-                                                                  src="{{$teacher->image }}" alt=""
-                                                                  class="w-100">
+                                    <div class="teacher-img"><img
+                                            style="width: 235px !important; height: 425px; !important;"
+                                            src="{{$teacher->image }}" alt=""
+                                            class="w-100">
 
                                     </div>
                                     <div class="teacher-info">
@@ -81,7 +82,7 @@
                                 <!--teacher end-->
                             </div>
 
-                         @endif
+                        @endif
                     @endforeach
                 </div>
                 <!--teachers end-->
@@ -91,9 +92,9 @@
                 <nav aria-label="Page navigation example">
                     <ul class="pagination">
                         @if((count( \App\Models\Teacher::where('school_id',env('SCHOOL_ID'))->get()->ToArray() )>$count))
-                        <li class="page-item"><a wire:click="viewMore" role="button" type="button"
-                                                 class="page-link">Yana</a>
-                        </li>
+                            <li class="page-item"><a wire:click="viewMore" role="button" type="button"
+                                                     class="page-link">Yana</a>
+                            </li>
                         @endif
 
                     </ul>
