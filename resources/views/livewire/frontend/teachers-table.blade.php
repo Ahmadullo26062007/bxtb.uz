8125b59b82ad471e20800b0221cc721f787e86c4
Ahmadullo26062007 <113325761+Ahmadullo26062007@users.noreply.github.com>
2023-08-04 21:48:44 +0500

    bxtb teachers finish1

--- resources/views/livewire/frontend/teachers-table.blade.php
+++ resources/views/livewire/frontend/teachers-table.blade.php
@@ -26,7 +26,7 @@
                                     {{-- @dd($teacher)--}}
                                     <div class="class-thumb"><img src="{{"$teacher->image"}}"
                                                                   alt="Student's class image"
-                                                                  class="w-100 h-100">
+                                                                  style="width: 200px; height: 100px">
                                     </div>
                                     <div class="class-info">
                                         <p>
