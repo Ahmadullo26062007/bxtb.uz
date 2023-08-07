2f00e2120fa85c5bbe212a0582092bf2624b5e7d
Ahmadullo26062007 <113325761+Ahmadullo26062007@users.noreply.github.com>
2023-08-07 17:07:51 +0500

    footer position bo'ldi3

--- database/seeders/TeacherSeeder.php
+++ database/seeders/TeacherSeeder.php
@@ -14,11 +14,26 @@ class TeacherSeeder extends Seeder
     public function run(): void
     {
         Teacher::insert([
-            ['firstname'=>"jeny",'lastname'=>"jenies",'category'=>'matematika','image'=>'20492370.jpg','created_at'=>'2023-06-26 15:33:47','school_id'=>1],
-            ['firstname'=>"jeny1",'lastname'=>"jenies1",'category'=>'inglis tili','image'=>'20492370.jpg','created_at'=>'2023-06-26 15:33:47','school_id'=>1],
-            ['firstname'=>"jeny2",'lastname'=>"jenies2",'category'=>'rus tili','image'=>'20492370.jpg','created_at'=>'2023-06-26 15:33:47','school_id'=>1],
-            ['firstname'=>"jeny3",'lastname'=>"jenies3",'category'=>'ona tili','image'=>'20492370.jpg','created_at'=>'2023-06-26 15:33:47','school_id'=>1],
-            ['firstname'=>"jeny4",'lastname'=>"jenies4",'category'=>'fizika','image'=>'20492370.jpg','created_at'=>'2023-06-26 15:33:47','school_id'=>1]
+            ['firstname' => "Dilorom", 'lastname' => "Erkinova", 'category' => 'Matematika', 'created_at' => '2023-06-26 15:33:47', 'school_id' => 2, 'image' => 'https://bxtb.uz/images/teacher.jpeg'],
+            ['firstname' => "Shahnoza", 'lastname' => "Zokirova", 'category' => 'Inglis Tili', 'created_at' => '2023-06-26 15:33:47', 'school_id' => 2, 'image' => 'https://bxtb.uz/images/teacher.jpeg'],
+            ['firstname' => "Surayyo", 'lastname' => "Odinova", 'category' => 'Rus Tili', 'created_at' => '2023-06-26 15:33:47', 'school_id' => 2, 'image' => 'https://bxtb.uz/images/teacher.jpeg'],
+            ['firstname' => "Dildora", 'lastname' => "Azimova", 'category' => 'Ona Tili', 'created_at' => '2023-06-26 15:33:47', 'school_id' => 2, 'image' => 'https://bxtb.uz/images/teacher.jpeg'],
+            ['firstname' => "Muyassarxon", 'lastname' => "Dexqonova", 'category' => 'Rus Tili', 'created_at' => '2023-06-26 15:33:47', 'school_id' => 3, 'image' => 'https://bxtb.uz/images/teacher.jpeg'],
+            ['firstname' => "Shahlo", 'lastname' => "Shodiyeva", 'category' => 'Inglis Tili', 'created_at' => '2023-06-26 15:33:47', 'school_id' => 3, 'image' => 'https://bxtb.uz/images/teacher.jpeg'],
+            ['firstname' => "Muhayyo", 'lastname' => "Qodirova", 'category' => 'Rus Tili', 'created_at' => '2023-06-26 15:33:47', 'school_id' => 3, 'image' => 'https://bxtb.uz/images/teacher.jpeg'],
+            ['firstname' => "Dilorom", 'lastname' => "Vohidova", 'category' => 'Ona Tili', 'created_at' => '2023-06-26 15:33:47', 'school_id' => 3, 'image' => 'https://bxtb.uz/images/teacher.jpeg'],
+            ['firstname' => "Nargiza", 'lastname' => "Erkinova", 'category' => 'Matematika', 'created_at' => '2023-06-26 15:33:47', 'school_id' => 4, 'image' => 'https://bxtb.uz/images/teacher.jpeg'],
+            ['firstname' => "Xalima", 'lastname' => "Zokirova", 'category' => 'Inglis Tili', 'created_at' => '2023-06-26 15:33:47', 'school_id' => 4, 'image' => 'https://bxtb.uz/images/teacher.jpeg'],
+            ['firstname' => "Ziyoda", 'lastname' => "Asqarova", 'category' => 'Rus Tili', 'created_at' => '2023-06-26 15:33:47', 'school_id' => 4, 'image' => 'https://bxtb.uz/images/teacher.jpeg'],
+            ['firstname' => "Xonzoda", 'lastname' => "Sultonova", 'category' => 'Ona Tili', 'created_at' => '2023-06-26 15:33:47', 'school_id' => 4, 'image' => 'https://bxtb.uz/images/teacher.jpeg'],
+            ['firstname' => "Dilafruz", 'lastname' => "Abdussatorova", 'category' => 'Matematika', 'created_at' => '2023-06-26 15:33:47', 'school_id' => 5, 'image' => 'https://bxtb.uz/images/teacher.jpeg'],
+            ['firstname' => "Shodiya", 'lastname' => "Abdukarimova", 'category' => 'Inglis Tili', 'created_at' => '2023-06-26 15:33:47', 'school_id' => 5, 'image' => 'https://bxtb.uz/images/teacher.jpeg'],
+            ['firstname' => "Zuhra", 'lastname' => "Odiljonova", 'category' => 'Rus Tili', 'created_at' => '2023-06-26 15:33:47', 'school_id' => 5, 'image' => 'https://bxtb.uz/images/teacher.jpeg'],
+            ['firstname' => "Fotima", 'lastname' => "Odiljonova", 'category' => 'Ona Tili', 'created_at' => '2023-06-26 15:33:47', 'school_id' => 5, 'image' => 'https://bxtb.uz/images/teacher.jpeg'],
+            ['firstname' => "Salima", 'lastname' => "Adukarimova", 'category' => 'Matematika', 'created_at' => '2023-06-26 15:33:47', 'school_id' => 6, 'image' => 'https://bxtb.uz/images/teacher.jpeg'],
+            ['firstname' => "Yulduz", 'lastname' => "Qohhorova", 'category' => 'Inglis Tili', 'created_at' => '2023-06-26 15:33:47', 'school_id' => 6, 'image' => 'https://bxtb.uz/images/teacher.jpeg'],
+            ['firstname' => "Gulsanam", 'lastname' => "Xojiyeva", 'category' => 'Rus Tili', 'created_at' => '2023-06-26 15:33:47', 'school_id' => 6, 'image' => 'https://bxtb.uz/images/teacher.jpeg'],
+            ['firstname' => "Oydin", 'lastname' => "Azimova", 'category' => 'Ona Tili', 'created_at' => '2023-06-26 15:33:47', 'school_id' => 6, 'image' => 'https://bxtb.uz/images/teacher.jpeg'],
         ]);
     }
 }
