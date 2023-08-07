3528b0f85cde6761b106985c965e3a0d5b0746a1
abduraxmon <abduraxmon.jalolov12@gmail.com>
2023-08-07 16:30:56 +0500

    erer1

--- resources/views/livewire/frontend/teachers-table.blade.php
+++ resources/views/livewire/frontend/teachers-table.blade.php
@@ -11,8 +11,8 @@
         <div class="row">
             @foreach ($teachers as $teacher)
                 @if($teacher->great_teacher==1 && $teacher->degrees )
-                    @dd($teacher->degrees[0])
-                    @if($teacher->degrees->type_id==1 )
+
+                    @if($teacher->degrees[0]->type_id==1 )
 
 
                     <div class="col-lg-3 col-md-6 col-sm-6">
@@ -75,7 +75,7 @@
         <div class="row">
             @foreach ($teachers as $teacher)
                 @if($teacher->great_teacher==1 && $teacher->degrees )
-                    @if( $teacher->degrees->type_id==1)
+                    @if( $teacher->degrees[0]->type_id==1)
 
 
                     <div class="col-lg-3 col-md-6 col-sm-6">
