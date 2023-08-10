ce11d7ff78312b5693daf5d382c3e1d6dd02a936
Ahmadullo26062007 <ahmadullo0039@gmail.com>
2023-08-10 19:10:58 +0500

    ddwedwdedwd

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -94,42 +94,42 @@
                 <div class="classes-sec">
                     <div class="row">
                         @foreach ($teachers as $teacher)
-                            @if($teacher->degrees)
-                            @if($teacher->great_teacher==1 && $teacher->degrees[0]->type_id==1)
-                                <div class="col-lg-3 col-md-6 col-sm-6">
-                                    <div class="classes-col">
-                                        <div class="class-thumb"><img src="{{"$teacher->image"}}"
-                                                                      alt="Student's class image"
-                                                                      style="width: 277px; height: 100px;">
-                                        </div>
-                                        <div class="class-info">
-                                            <p>@php
-                                                    $ab=\App\Models\About::find($teacher->school_id);
-                                                @endphp
-                                                {{$ab->name}} O'qtuvchisi
-                                            </p>
-                                            <h3>{{ $teacher->firstname }} {{$teacher->lastname}}
-                                            </h3>
-                                            <span>{{$teacher->category}} O'qtuvchisi</span>
-                                            <h5>
-                                                @if (empty($teacher->degrees[0]))
-                                                    <span class="text-dark">
+                            @foreach($teacher->degrees as $deg)
+                                @if($teacher->great_teacher==1 && $tdeg->type_id==1)
+                                    <div class="col-lg-3 col-md-6 col-sm-6">
+                                        <div class="classes-col">
+                                            <div class="class-thumb"><img src="{{"$teacher->image"}}"
+                                                                          alt="Student's class image"
+                                                                          style="width: 277px; height: 100px;">
+                                            </div>
+                                            <div class="class-info">
+                                                <p>@php
+                                                        $ab=\App\Models\About::find($teacher->school_id);
+                                                    @endphp
+                                                    {{$ab->name}} O'qtuvchisi
+                                                </p>
+                                                <h3>{{ $teacher->firstname }} {{$teacher->lastname}}
+                                                </h3>
+                                                <span>{{$teacher->category}} O'qtuvchisi</span>
+                                                <h5>
+                                                    @if (empty($teacher->degrees[0]))
+                                                        <span class="text-dark">
                                                             2-foifali O'qtuvchi
                                                         </span>
-                                                @else
-                                                    @foreach ($teacher->degrees as $degree)
-                                                        <span class="text-dark">
+                                                    @else
+                                                        @foreach ($teacher->degrees as $degree)
+                                                            <span class="text-dark">
                                                     {{ App\Models\Degree::TYPES[$degree->type_id] }}
                                                 </span>
-                                                    @endforeach
-                                                @endif
-                                            </h5>
+                                                        @endforeach
+                                                    @endif
+                                                </h5>
+                                            </div>
                                         </div>
+                                        <!--classes-col end-->
                                     </div>
-                                    <!--classes-col end-->
-                                </div>
-                            @endif
-                            @endif
+                                @endif
+                            @endforeach
                         @endforeach
                     </div>
                 </div>
