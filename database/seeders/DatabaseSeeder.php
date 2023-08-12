ca8973fb06244ff71519d3ec5a1b23b0d5c4a2df
Ahmadullo26062007 <ahmadullo0039@gmail.com>
2023-08-12 10:02:49 +0500

    ewe

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,34 +16,34 @@ class DatabaseSeeder extends Seeder
 
 
          $this->call([
-//             CategorySeeder::class,
-//             WeekSeeder::class,
-//             AboutSeeder::class,
+             CategorySeeder::class,
+             WeekSeeder::class,
+             AboutSeeder::class,
              TeacherSeeder::class,
              ClassesSeeder::class,
-//             CourseSeeder::class,
-//             BlogSeeder::class,
+             CourseSeeder::class,
+             BlogSeeder::class,
              StudentSeeder::class,
 //             TypeSeeder::class
          ]);
-//        \App\Models\User::create([
-//            'name' => 'SeniorAdmin',
-//            'username' => 'owner',
-//            'password'=>bcrypt('owner1505'),
-//            'school_id'=>null
-//        ]);
+        \App\Models\User::create([
+            'name' => 'SeniorAdmin',
+            'username' => 'owner',
+            'password'=>bcrypt('owner1505'),
+            'school_id'=>null
+        ]);
 //        \App\Models\User::create([
 //            'name' => 'Admin',
 //            'username' => 'admin',
 //            'password'=>bcrypt('password'),
 //            'school_id'=>1
 //        ]);
-//        foreach (Course::all() as $k=>$c) {
-//            if ($k%2==0){
-//                $c->weeks()->sync([1,3,5]);
-//            }else{
-//                $c->weeks()->sync([2,4,6]);
-//            }
-//        }
+        foreach (Course::all() as $k=>$c) {
+            if ($k%2==0){
+                $c->weeks()->sync([1,3,5]);
+            }else{
+                $c->weeks()->sync([2,4,6]);
+            }
+        }
     }
 }
