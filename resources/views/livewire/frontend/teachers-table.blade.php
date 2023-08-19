9c2c62f79bbe6238816db021e5654278a94339a8
Yahyobek <mywhite060608@gmail.com>
2023-08-19 16:07:40 +0500

    fixing bugs in teacher-table

--- resources/views/livewire/frontend/teachers-table.blade.php
+++ resources/views/livewire/frontend/teachers-table.blade.php
@@ -61,7 +61,7 @@
                     <h1 style="color: #0a0a0a;font-size: 30px; font-weight: 500;  margin-bottom:5%;">1-toifalilar
                     </h1>
                 </div>
-                @if (!$teacher->great_teacher)
+                @if (!$teachers)
                     <div class="col-12 d-flex justify-content-center">
                         <h1 style="color: #ff0000;font-size: 30px; font-weight: 500;  margin-bottom:5%;">Mavjud emas
                         </h1>
